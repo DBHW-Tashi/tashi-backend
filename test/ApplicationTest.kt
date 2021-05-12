3c9c7588e6820b08217664bb71b55859f5665054
thedeathofpotato <renner.j.992@gmail.com>
2021-05-12 15:51:41 +0200

    belongs to previous comit

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -16,7 +16,7 @@ class ApplicationTest {
         withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLD!", response.content)
+                assertEquals("Testroute Called!", response.content)
             }
         }
     }
