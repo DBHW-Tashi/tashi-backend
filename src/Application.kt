7478d8d6130308765f52c5fba7da64ac14e690a8
thedeathofpotato <renner.j.992@gmail.com>
2021-05-12 15:46:35 +0200

    prototype Compute Function added             changed Test

--- src/Application.kt
+++ src/Application.kt
@@ -1,12 +1,16 @@
 package com.renner
 
 import io.ktor.application.*
+import io.ktor.jackson.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.features.*
 import org.slf4j.event.*
 import io.ktor.routing.*
 import io.ktor.http.*
+
+data class computeData(val exp: String, val val_1: String, val val_2: String)
+data class returnData(var result: String)
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -17,11 +21,32 @@ fun Application.module(testing: Boolean = false) {
         level = Level.INFO
         filter { call -> call.request.path().startsWith("/") }
     }
+    install(ContentNegotiation) {
+        jackson()
+    }
 
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            call.respondText("Testroute Called!", contentType = ContentType.Text.Plain)
+        }
+        post(path = "/compute"){
+            val requestData = call.receive<computeData>()
+            call.respond(compute(requestData))
         }
     }
+}
+
+fun compute(inData: computeData): returnData {
+    val var_1 = inData.val_1.toDouble()
+    val var_2 = inData.val_2.toDouble()
+    var outData = returnData("")
+    when{
+        inData.exp.contains('+') -> {outData.result = (var_1+var_2).toString()}
+        inData.exp.contains('-') -> {outData.result = (var_1-var_2).toString()}
+        inData.exp.contains('*') -> {outData.result = (var_1*var_2).toString()}
+        inData.exp.contains('/') -> {outData.result = (var_1/var_2).toString()}
+
+    }
+    return outData
 }
 
