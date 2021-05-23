5af293f7a47149f8f721a352e1bf06ffeb2991ec
Jakob Renner <renner.j.992@gmail.com>
2021-05-23 10:26:50 +0200

    Test Commit with partial work

--- src/AbsolutNumber.kt
+++ src/AbsolutNumber.kt
@@ -1,33 +1,39 @@
 package com.renner
 
 class AbsolutNumber(var CreationString: String, val base: Int) {
-    var Values: UByteArray = UByteArray(1);
-    var Sing: Boolean = true;
+    var digits: UByteArray;
+    var sing: Boolean = true;
 
     init {
         if (this.CreationString.substring(0, 1) == "-") {
-            this.Sing = false;
+            this.sing = false;
             this.CreationString = this.CreationString.trim { it == '-' || it <= ' ' }  //trim whitespace and -
         }
-        this.Values = UByteArray(this.CreationString.length)  //Klappt solange 1 stelle Creation string 1 UByte entspricht
-        if (this.base > 10) {
+        this.digits = UByteArray(this.CreationString.length)  //Klappt solange 1 stelle Creation string 1 UByte entspricht
+        if (this.base != 10) {
             throw Error("not yet implemented");
         } else {
             var i:Int = 0;
             for(curChar: Char in this.CreationString) {
-                Values.set(i, Character.getNumericValue(curChar).toUByte())
+                digits.set(i, Character.getNumericValue(curChar).toUByte())
                 i++;
             }
         }
+    }
 
+    operator fun plus(b : AbsolutNumber):AbsolutNumber{
+        var i = 0;
+        while(true){
+            if(this.digits[i] + b.digits[i] < this.base.toUInt())
+        }
     }
 
     override fun toString(): String {
         var resString: String = "";
-        if(!this.Sing){
+        if(!this.sing){
             resString = "-"
         }
-        for(pos in this.Values){
+        for(pos in this.digits){
             resString += pos.toString();
         }
         return resString
